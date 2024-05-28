f5aa3c5bcd5cf5eec671125f4d28c2a1a50a9bce
kevin120202 <kevindelapaz21@gmail.com>
2024-05-28 11:23:34 -0500

    added form questions

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { decode } from 'html-entities';
 
 export default function Question(props) {
-    console.log(props);
+
     return (
         <div>
-            <p>{props.question.question}</p>
+            <p>{decode(props.question.question, { level: 'html5' })}</p>
+            {props.question.randomAnswers.map((answer, index) => (
+                <div key={index}>
+                    <input
+                        type="radio"
+                        id={`${props.question.id}-${index}`}
+                        name={props.question.id}
+                        value={answer}
+                        checked={props.question.chosenAnswer === answer}
+                        onChange={() => props.handleAnswerChange(props.question.id, answer)}
+                    />
+                    <label htmlFor={`${props.question.id}-${index}`}>{answer}</label>
+                </div>
+            ))}
         </div>
     )
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,28 +4,64 @@ import Question from "./Question";
 
 export default function Quiz() {
     const [quizQuestions, setQuizQuestions] = useState([])
+    const [isSubmit, setIsSubmit] = useState(false)
+    const [score, setScore] = useState(0)
 
     useEffect(() => {
-        fetch("https://opentdb.com/api.php?amount=5&category=21&difficulty=medium&type=multiple")
-            .then(res => res.json())
-            .then(data => {
-                setQuizQuestions(data.results)
-            })
+        const fetchQuestions = async () => {
+            const res = await fetch("https://opentdb.com/api.php?amount=5&category=21&difficulty=medium&type=multiple")
+            const data = await res.json()
+            setQuizQuestions(data.results.map(result => ({
+                question: result.question,
+                correctAnswer: result.correct_answer,
+                randomAnswers: getRandomAnswersArray([...result.incorrect_answers], result.correct_answer),
+                id: uuidv4(),
+                chosenAnswer: "none",
+            })))
+        }
+        fetchQuestions()
     }, [])
 
-    console.log(quizQuestions);
+    const getRandomAnswersArray = (array, item) => {
+        const randomIndex = Math.floor(Math.random() * (array.length + 1))
+        array.splice(randomIndex, 0, item)
+        return array
+    }
+
+    const handleAnswerChange = (id, answer) => {
+        setQuizQuestions(prevQuestion => {
+            return prevQuestion.map(question =>
+                question.id === id ? { ...question, chosenAnswer: answer } : question
+            )
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let newScore = 0
+        quizQuestions.forEach(question => {
+            if (question.correctAnswer === question.chosenAnswer) {
+                newScore += 1
+            }
+        })
+        setScore(newScore)
+        setIsSubmit(prev => !prev)
+    }
 
     const questionsComponents = quizQuestions.map(question => {
-        return <Question question={question} key={uuidv4()} />
+        return <Question question={question} key={question.id} handleAnswerChange={handleAnswerChange} />
     })
 
-    console.log(questionsComponents);
-
+    console.log(quizQuestions);
 
     return (
         <div>
             <h1>Quiz</h1>
-            {questionsComponents}
+            <form onSubmit={handleSubmit}>
+                {questionsComponents}
+                {isSubmit && <p>You scored {score}/5 correct answers</p>}
+                {isSubmit ? <button>Play again</button> : <button>Submit</button>}
+            </form>
         </div>
     )
 }
